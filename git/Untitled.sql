849901baed20c2762089e59ac95e59bca84d4180
T0334910-Ahmed <t0334910@my.ntu.ac.uk>
2024-06-11 10:26:26 +0100

    Creating the database
    This code represent the database of the assingmnet and the passenger table

---
+++ git/Untitled.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE Sunderland_cruise_ships;
+USE  Sunderland_cruise_ships;
+
+CREATE TABLE Passenger (
+    Passenger_ID INT PRIMARY KEY,
+    Passenger_name VARCHAR(50),
+    Cabin INT
+);
